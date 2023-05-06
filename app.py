197b15ea45a1dc5a49754129c3d0bd38311bf467
key <30455795+keyhsw@users.noreply.github.com>
2023-05-06 19:20:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,16 @@ description = """<h3>This is the demo of MiniGPT-4. Upload your images and start
 article = """<div style='display:flex; gap: 0.25rem; '><a href='https://minigpt-4.github.io'><img src='https://img.shields.io/badge/Project-Page-Green'></a><a href='https://github.com/Vision-CAIR/MiniGPT-4'><img src='https://img.shields.io/badge/Github-Code-blue'></a><a href='https://github.com/TsuTikgiau/blip2-llm/blob/release_prepare/MiniGPT_4.pdf'><img src='https://img.shields.io/badge/Paper-PDF-red'></a></div>
 """
 
-with gr.Blocks() as demo:
-    gr.Markdown(title)
-#     gr.Markdown(SHARED_UI_WARNING)
-    gr.Markdown(description)
-    gr.Markdown(article)
+# with gr.Blocks() as demo:
+#     gr.Markdown(title)
+# #     gr.Markdown(SHARED_UI_WARNING)
+#     gr.Markdown(description)
+#     gr.Markdown(article)
 
 demo.Interface(
     predict,
     inputs=gr.inputs.Image(label="Upload hot dog candidate", type="filepath"),
     outputs=gr.outputs.Label(num_top_classes=2),
-#     title="Hot Dog? Or Not?"
+    title="Hot Dog? Or Not?",
+    article = article,
 ).launch()
