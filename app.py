b8f3a810e6d9993bb669d55f5454405bcc084484
key <30455795+keyhsw@users.noreply.github.com>
2023-07-31 14:17:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ import gradio as gr
 import os
 from transformers import pipeline
 
+# from openxlab.model import download
+# download(model_repo='thomas-yanxin/MindChat-InternLM-7B', model_name=['pytorch_model-00001-of-00002.bin'])
+
 from openxlab.model import download
-download(model_repo='thomas-yanxin/MindChat-InternLM-7B', model_name=['pytorch_model-00001-of-00002.bin'])
+download(model_repo='thomas-yanxin/MindChat-InternLM-7B', 
+model_name='pytorch_model-00002-of-00002.bin')
 
 pipeline = pipeline(task="image-classification", model="julien-c/hotdog-not-hotdog")
 def predict(image):
